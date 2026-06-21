2a38d4c89db91907888c7f3a940a25c8af8ee945
Danila <danilazmiter@gmail.com>
2026-06-21 14:40:41 +0300

    Minimal main endpoint

--- app/api/main.py
+++ app/api/main.py
@@ -1,3 +1,13 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"name": "Root View"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
