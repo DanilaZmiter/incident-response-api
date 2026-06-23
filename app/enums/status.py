d679a23e40b83975a5fa09db6d49a0d8fa8781ea
Danila <danilazmiter@gmail.com>
2026-06-24 01:10:18 +0300

    Better look with balck formatter(i forgot to do it before commit))))

--- app/enums/status.py
+++ app/enums/status.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+
 class Status(Enum):
     active = "active"
     non_active = "non-active"
--- app/models/incident.py
+++ app/models/incident.py
@@ -3,12 +3,14 @@ from sqlalchemy import Integer, ForeignKey, String, DateTime, Enum
 from database.base import BaseModel
 from enums.status import Status
 from typing import TYPE_CHECKING
+
 if TYPE_CHECKING:
     from models.user import UserModel
 
+
 class IncidentModel(BaseModel):
     __tablename__ = "incidents"
-    
+
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
     title: Mapped[str] = mapped_column(String(40), nullable=False)
