9949afb4f08dee7c100cae8315697ada11dc6614
Danila <danilazmiter@gmail.com>
2026-06-24 01:07:45 +0300

    Two enum classes with roles of users and statuses of incidents

---
+++ app/enums/roles.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Roles(Enum):
+    guest = "guest"
+    user = "user"
+    administrator = "administrator"
---
+++ app/enums/status.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Status(Enum):
+    active = "active"
+    non_active = "non-active"
+    in_procces = "in-procces"
