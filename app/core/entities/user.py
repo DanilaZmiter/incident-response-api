d9eb01aed294cabcab66f76efc3a85309d0e9789
Danila <danilazmiter@gmail.com>
2026-06-25 14:28:20 +0300

    Create domain entities classes(Base and user)

---
+++ app/core/entities/user.py
@@ -0,0 +1,13 @@
+from app.core.entities.base import BaseEntity
+from dataclasses import dataclass
+from app.core.enums.roles import Roles
+
+@dataclass
+class UserEntity(BaseEntity):
+    id: str
+    username: str
+    mail: str
+    hashed_password: str
+    is_active: bool
+    role: Roles
+    
